55f9e254a69147c635e172d4fa45b8536320f7f1
coderGLM <779793115@qq.com>
2016-10-20 06:22:28 +0800

    move app.vue to src/components

---
+++ src/components/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="wrapper">
+    <div id="header">
+      <h1>Header</h1>
+    </div>
+    <div>
+      <a href="#/home/123">home</a>
+      <a href="#/login/123">login</a>
+      <a href="#/about/123">about</a>
+    </div>
+    <router-view
+      class="view"
+      keep-alive
+      transition
+      transition-mode="out-in">
+    </router-view>
+  </div>
+</template>
+
