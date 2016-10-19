d8861892e2e7d89a3b9ab6f20bdce23acf618bba
coderGLM <779793115@qq.com>
2016-10-20 06:19:57 +0800

    add about page

---
+++ src/components/AboutPage.vue
@@ -0,0 +1,30 @@
+<template>
+<div>{{ desc }}</div>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage',
+  data () {
+    return {
+      desc: 'This is a AboutPage'
+    }
+  },
+  route: {
+    data ({ to }) {
+      document.title = to.params.id
+      return {
+        desc: 'Enter AboutPage'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+$fontSize: 20px;
+
+div {
+  font-size: $fontSize
+}
+</style>
