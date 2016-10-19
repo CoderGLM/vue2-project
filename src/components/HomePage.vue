ea89d371a67acbfc8108854d8bfcff68a71c7648
coderGLM <779793115@qq.com>
2016-10-20 06:22:00 +0800

    add home and login page

---
+++ src/components/HomePage.vue
@@ -0,0 +1,30 @@
+<template>
+<div>{{ desc }}</div>
+</template>
+
+<script>
+export default {
+  name: 'HomePage',
+  data () {
+    return {
+      desc: 'This is a HomePage'
+    }
+  },
+  route: {
+    data ({ to }) {
+      document.title = to.params.id
+      return {
+        desc: 'Enter HomePage'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+$fontSize: 20px;
+
+div {
+  font-size: $fontSize
+}
+</style>
---
+++ src/components/LoginPage.vue
@@ -0,0 +1,30 @@
+<template>
+<div>{{ desc }}</div>
+</template>
+
+<script>
+export default {
+  name: 'LoginPage',
+  data () {
+    return {
+      desc: 'This is a LoginPage'
+    }
+  },
+  route: {
+    data ({ to }) {
+      document.title = to.params.id
+      return {
+        desc: 'Enter LoginPage'
+      }
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+$fontSize: 20px;
+
+div {
+  font-size: $fontSize
+}
+</style>
