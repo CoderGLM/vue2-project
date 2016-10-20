44064b11bdbd8b4518601f640959624b98d17a45
ZAIJIANLUOYE110 <779793115@qq.com>
2016-10-20 17:14:11 +0800

    add header to App.vue

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,8 +1,6 @@
 <template>
   <div id="wrapper">
-    <div id="header">
-      <h1>Header</h1>
-    </div>
+    <header>CoderGLM</header>
     <div>
       <a href="#/home/123">home</a>
       <a href="#/login/123">login</a>
@@ -17,3 +15,17 @@
   </div>
 </template>
 
+<style lang="scss">
+  header {
+    box-sizing: border-box;
+    width: 100%;
+    height: 3rem;
+    padding: 0.7rem;
+    font-family: helvetica, arial, sans-serif;
+    font-size: 1.4rem;
+    font-weight: bold;
+    text-align: center;
+    color: #fff;
+    background: #3b5998;
+  }
+</style>
