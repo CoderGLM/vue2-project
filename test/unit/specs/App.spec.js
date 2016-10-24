82e779b322f22ad7be61db3d8cd57ceb08f52a46
ZAIJIANLUOYE110 <779793115@qq.com>
2016-10-24 14:37:15 +0800

    unit test has been changed to jasmine from mocha

--- test/unit/specs/App.spec.js
+++ test/unit/specs/App.spec.js
@@ -1,13 +1,26 @@
 //import Vue from 'vue'
-//import Hello from 'src/components/Hello'
+//import App from 'src/components/App'
 //
-//describe('Hello.vue', () => {
+//describe('App.vue', () => {
 //  it('should render correct contents', () => {
 //    const vm = new Vue({
 //      el: document.createElement('div'),
-//      render: (h) => h(Hello)
+//      render: (h) => h(App)
 //    })
-//    expect(vm.$el.querySelector('.hello h1').textContent)
-//      .to.equal('Welcome to Your Vue.js App')
+//    expect(vm.$el.querySelector('a').textContent)
+//      .to.equal('home')
 //  })
 //})
+import Vue from 'vue'
+import App from 'src/components/App'
+
+describe('App.vue', () => {
+  it('should render correct contents', () => {
+    const vm = new Vue({
+      el: document.createElement('div'),
+      render: (h) => h(App)
+    })
+    expect(vm.$el.querySelector('a').textContent)
+      .toBe('home')
+  })
+})
