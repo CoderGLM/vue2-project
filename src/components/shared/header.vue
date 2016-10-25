b1068c8e3cae4f6b5b9fef1be2b4aa42994fb005
ZAIJIANLUOYE110 <779793115@qq.com>
2016-10-25 14:43:52 +0800

    增加公共组件header

---
+++ src/components/shared/header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <slot></slot>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'PageHeader'
+}
+</script>
+
+<style lang="scss" scoped>
+header {
+  padding: 0.7rem;
+  font-family: helvetica, arial, sans-serif;
+  font-size: 1.4rem;
+  font-weight: bold;
+  text-align: center;
+  color: #fff;
+  background: #3b5998;
+}
+</style>
