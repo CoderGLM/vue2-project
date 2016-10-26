cc62c2634c912900b704fbb8c5e4fad0f261b755
ZAIJIANLUOYE110 <779793115@qq.com>
2016-10-26 14:24:59 +0800

    rename from header.vue to PageHeader.vue

---
+++ src/components/shared/PageHeader.vue
@@ -0,0 +1,28 @@
+<template>
+  <header>
+    <slot></slot>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'PageHeader'
+}
+</script>
+
+<style lang="scss" scoped>
+@import "./src/assets/scss/_variables.scss";
+
+header {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 0.7rem;
+  font-size: $font-size-lg;
+  color: #fff;
+  font-family: $font-family-sans-serif;
+  font-weight: bold;
+  text-align: center;
+  background: #3b5998;
+}
+</style>
