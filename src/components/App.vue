6e948ead6de8cdaed3e682fe51e993e60a18f1d7
ZAIJIANLUOYE110 <779793115@qq.com>
2016-10-27 17:33:06 +0800

    try vuex

--- src/components/App.vue
+++ src/components/App.vue
@@ -11,6 +11,23 @@
 <script>
 import 'assets/scss/reset.scss'
 import 'assets/scss/style.scss'
+import Vuex from 'vuex'
+
+const store = new Vuex.Store({
+  state: {
+    count: 1
+  },
+  mutations: {
+    increment (state) {
+      state.count++
+    }
+  }
+})
+
+export default {
+  name: 'App',
+  store
+}
 </script>
 
 <style lang="scss">
