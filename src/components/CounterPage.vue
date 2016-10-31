daa3ca1d21828885634f04a7cbe9fccb31f81387
coderGLM <779793115@qq.com>
2016-10-31 10:28:34 +0800

    添加CounterPage-(用于测试vuex)

---
+++ src/components/CounterPage.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div>{{ num }}</div>
+    <div>{{ isBig }}</div>
+    <button @click="addAction">Add</button>
+    <button @click="subAction">Sub</button>
+    <button @click="resetAction">RESET</button>
+  </div>
+</template>
+
+<script>
+import { mapState, mapGetters, mapActions } from 'vuex'
+
+export default {
+  computed: {
+    ...mapState({
+      // Note that: counter in module1
+      num: state => state.module1.counter
+    }),
+    ...mapGetters({
+      isBig: 'isBigger'
+    })
+  },
+  methods: {
+    ...mapActions([
+      'addAction',
+      'subAction',
+      'resetAction'
+    ])
+  }
+}
+</script>
