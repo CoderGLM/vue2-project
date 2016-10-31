5d02fd7eb3dc00479b9768f94418186bb2aba3c0
coderGLM <779793115@qq.com>
2016-10-31 10:34:05 +0800

    增加Vue.use(Vuex)

--- src/store/index.js
+++ src/store/index.js
@@ -1,3 +1,4 @@
+import Vue from 'vue'
 import Vuex from 'vuex'
 import * as actions from './actions'
 import * as getters from './getters'
@@ -5,6 +6,8 @@ import module1 from './modules/module1'
 import module2 from './modules/module2'
 
 const debug = process.env.NODE_ENV !== 'production'
+
+Vue.use(Vuex)
 
 export default new Vuex.Store({
   actions,
