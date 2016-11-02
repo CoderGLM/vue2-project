15e3ff385424f3b1d9362ab5467816c4a5cc017d
ZAIJIANLUOYE110 <779793115@qq.com>
2016-11-02 12:37:49 +0800

    加header

--- src/components/CounterPage.vue
+++ src/components/CounterPage.vue
@@ -1,5 +1,10 @@
 <template>
-  <div>
+  <div id="wrapper">
+    <page-header>
+      <button class="page-header-btn" onclick="history.go(-1)">&lt;back</button>
+      <span class="page-header-title">vuex test</span>
+      <button class="page-header-btn"></button>
+    </page-header>
     <div>{{ num }}</div>
     <div>{{ isBig }}</div>
     <button @click="addAction">Add</button>
@@ -10,8 +15,12 @@
 
 <script>
 import { mapState, mapGetters, mapActions } from 'vuex'
+import PageHeader from './shared/PageHeader.vue'
 
 export default {
+  components: {
+    PageHeader
+  },
   computed: {
     ...mapState({
       // Note that: counter in module1
@@ -30,3 +39,19 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+@import "./src/assets/scss/_variables.scss";
+
+#wrapper {
+  width: 100%;
+}
+.page-header-title {
+  display: inline-block;
+  flex: 1;
+}
+.page-header-btn {
+  width: $height-top-menu;
+  color: #fff;
+}
+</style>
