aaec7391f0b5917efa6f7b9fd05d307d184a0762
ZAIJIANLUOYE110 <779793115@qq.com>
2016-11-02 20:09:49 +0800

    添加用户页面

---
+++ src/components/UserPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="wrapper">
+    <page-header>This is UserPage</page-header>
+  </div>
+</template>
+
+<script>
+import PageHeader from './shared/PageHeader'
+
+export default {
+  name: 'UserPage',
+  components: {
+    PageHeader
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+#wrapper {
+  width: 100%
+}
+</style>
