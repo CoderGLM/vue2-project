2738a626b1236b496541f5c0ba2921ce89609fba
ZAIJIANLUOYE110 <779793115@qq.com>
2016-11-02 19:52:38 +0800

    抽取api域名

--- src/const.js
+++ src/const.js
@@ -8,6 +8,8 @@
 export const clientID = '8fda6183ba583635f0e5'
 export const clientSecret = '6402f910e027326d07edda48ccb3e2573cb55a77'
 
+export const apiDomain = 'https://api.github.com'
+
 //  Page
 //
 //
@@ -18,4 +20,4 @@ export const authorizeUrl = `https://github.com/login/oauth/authorize?client_id=
 //
 //
 export const getAccessToken = 'http://localhost:8888/access_token/'
-export const getUserInfo = 'https://api.github.com/user?access_token='
+export const getUserInfo = `${apiDomain}/user?access_token=`
