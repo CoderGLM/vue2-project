9cdd76087b895b1c9e9c4b1de542abb75623a499
ZAIJIANLUOYE110 <779793115@qq.com>
2016-11-02 19:18:33 +0800

    添加常量文件

---
+++ src/const.js
@@ -0,0 +1,21 @@
+/*
+ *  定义常量
+ */
+
+//  OAuth Settings
+//
+//
+export const clientID = '8fda6183ba583635f0e5'
+export const clientSecret = '6402f910e027326d07edda48ccb3e2573cb55a77'
+
+//  Page
+//
+//
+// Authorize Page
+export const authorizeUrl = `https://github.com/login/oauth/authorize?client_id=${clientID}`
+
+// Api
+//
+//
+export const getAccessToken = 'http://localhost:8888/access_token/'
+export const getUserInfo = 'https://api.github.com/user?access_token='
