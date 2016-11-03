8ee784ab8087be372ece1ce5482bb1dc1ac8af3a
ZAIJIANLUOYE110 <779793115@qq.com>
2016-11-03 17:00:11 +0800

    调整star的样式

--- src/components/UserPage.vue
+++ src/components/UserPage.vue
@@ -55,7 +55,7 @@
           <svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 12 16" width="12"><path d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"></path></svg>
           <div class="repolist-item-title">CoderGLM/ReactNativeLeaning</div>
           <strong>
-            24
+            <span>24</span>
             <svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 14 16" width="14"><path d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74z"></path></svg>
           </strong>
          </a>
@@ -63,19 +63,11 @@
           <svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 12 16" width="12"><path d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"></path></svg>
           <div class="repolist-item-title">CoderGLM/ReactNativeLeaning</div>
           <strong>
-            24
+            <span>24</span>
             <svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 14 16" width="14"><path d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74z"></path></svg>
           </strong>
          </a>
-         <a class="repolist-item" href="http://gengliming.com">
-          <svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 12 16" width="12"><path d="M4 9H3V8h1v1zm0-3H3v1h1V6zm0-2H3v1h1V4zm0-2H3v1h1V2zm8-1v12c0 .55-.45 1-1 1H6v2l-1.5-1.5L3 16v-2H1c-.55 0-1-.45-1-1V1c0-.55.45-1 1-1h10c.55 0 1 .45 1 1zm-1 10H1v2h2v-1h3v1h5v-2zm0-10H2v9h9V1z"></path></svg>
-          <div class="repolist-item-title">CoderGLM/ReactNativeLeaning</div>
-          <strong>
-            24
-            <svg aria-hidden="true" height="16" version="1.1" viewBox="0 0 14 16" width="14"><path d="M14 6l-4.9-.64L7 1 4.9 5.36 0 6l3.6 3.26L2.67 14 7 11.67 11.33 14l-.93-4.74z"></path></svg>
-          </strong>
-         </a>
-      </div>
+       </div>
     </div>
 
     <div class="timeline-wrapper">
@@ -313,6 +305,11 @@ svg {
     >strong,>svg {
       min-width: 3rem;
       color: #767676;
+    }
+    >strong {
+      span {
+        vertical-align: text-top;
+      }
     }
     #{&}+.repolist-item {
       border-top: 1px solid $color-border;
