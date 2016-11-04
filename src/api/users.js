371541f77733b89895d118cfc16732f3f4ab1881
ZAIJIANLUOYE110 <779793115@qq.com>
2016-11-04 09:43:25 +0800

    mv api/ to src/api

---
+++ src/api/users.js
@@ -0,0 +1,10 @@
+//  Users Api
+//
+//  https://developer.github.com/v3/users/#get-the-authenticated-user
+//
+
+import * as Const from '../const'
+
+function userInfoWithToken(token) {
+
+}
--- src/const.js
+++ src/const.js
@@ -9,6 +9,7 @@ export const clientID = '8fda6183ba583635f0e5'
 export const clientSecret = '6402f910e027326d07edda48ccb3e2573cb55a77'
 
 export const apiDomain = 'https://api.github.com'
+export const transferDomain = 'http://localhost:8888'
 
 //  Page
 //
@@ -19,5 +20,5 @@ export const authorizeUrl = `https://github.com/login/oauth/authorize?client_id=
 // Api
 //
 //
-export const getAccessToken = 'http://localhost:8888/access_token/'
+export const getAccessToken = `${transferDomain}/access_token/`
 export const getUserInfo = `${apiDomain}/user?access_token=`
