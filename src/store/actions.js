04322734dd98b3da0cd5f4f2b059c3678b9ce4f0
ZAIJIANLUOYE110 <779793115@qq.com>
2016-11-04 17:33:06 +0800

    移除Add\Sub\Reset action

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,25 +1,13 @@
 import * as types from './types'
 import Api from 'src/api'
 
-export const addAction = ({ commit }) => {
-  commit(types.ADD, 1)
-}
-
-export const subAction = ({ commit }) => {
-  commit(types.SUB, 1)
-}
-
-export const resetAction = ({ commit }) => {
-  commit(types.reset)
-}
-
 /*
  *
  *  Users Module
  *
  */
-export const getAuthUserInfo = ({ commit }) => {
-  Api.users.getAuthUserInfo().then(json => {
+export const getAuthUserInfo = ({ commit }, token) => {
+  Api.users.getAuthUserInfo(token).then(json => {
     commit(types.GetAuthUserInfo, json)
   })
 }
