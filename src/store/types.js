0a32b44239375fa7dc49c18f86941280591a73fd
ZAIJIANLUOYE110 <779793115@qq.com>
2016-11-04 14:02:09 +0800

    移除CounterPage相关内容

--- src/store/types.js
+++ src/store/types.js
@@ -1,3 +1,10 @@
 export const ADD = 'namespace/ADD'
 export const SUB = 'namespace/SUB'
 export const RESET = 'namespace/RESET'
+
+/*
+ *
+ *  Users Module
+ *
+ */
+export const GetAuthUserInfo = 'namespace/GetAuthUserInfo'
