4bb5dbc5dda3583c73f8f2d08d9b8f909b52e49e
coderGLM <779793115@qq.com>
2016-11-05 11:27:18 +0800

    bug fixed: 超时访问

--- test/unit/specs/api/users.spec.js
+++ test/unit/specs/api/users.spec.js
@@ -1,15 +1,32 @@
+/*
+ *
+ *  When you test vue-resource, you must gurantee:
+ *
+ *  1. add Vue.use(Resource) or the Vue.http will be undefined
+ *
+ *  2. add catch to promise object or unexpected error will come to you.
+ *
+ *
+ */
+
+import Vue from 'vue'
+import Resource from 'vue-resource'
 import Api from 'src/api'
 
 describe('Api.users', () => {
   const { users } = Api
 
+  beforeEach(() => {
+    // This is required for `users.getAuthUserInfo` function
+    Vue.use(Resource)
+    window.jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000
+  })
+
   it('will get json data', done => {
-    users.getAuthUserInfo('552a0d2a68b89f6e13d0e07d71f831cd1797e7cd89').then(json => {
-      done(() => {
-        expect(typeof json).toBe('object')
-      })
-    }).catch(err => {
-      throw err
-    })
+    users.getAuthUserInfo('3f3c4d1ac7c55d89b48bcdbe6d519f5ba3167b78').then(json => {
+      expect(typeof json).toBe('object')
+    }).catch(error => {
+      expect(typeof error).tbBe('object')
+    }).finally(done)
   })
 })
