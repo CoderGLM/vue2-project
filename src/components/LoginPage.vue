e7131eec785f4e061de269eae8956e751866b29c
ZAIJIANLUOYE110 <779793115@qq.com>
2016-11-30 17:46:18 +0800

    抽取样式

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,7 +1,11 @@
 <template>
  <div id="wrapper bgf hc fwb">
   <page-header>
-    <h1>Github</h1>
+    <svg slot="left" aria-hidden="true" height="24" version="1.1" viewBox="0 0 12 16" width="18">
+      <path fill-rule="evenodd" d="M11.41 9H.59C0 9 0 8.59 0 8c0-.59 0-1 .59-1H11.4c.59 0 .59.41.59 1 0 .59 0 1-.59 1h.01zm0-4H.59C0 5 0 4.59 0 4c0-.59 0-1 .59-1H11.4c.59 0 .59.41.59 1 0 .59 0 1-.59 1h.01zM.59 11H11.4c.59 0 .59.41.59 1 0 .59 0 1-.59 1H.59C0 13 0 12.59 0 12c0-.59 0-1 .59-1z"></path>
+    </svg>
+    <h1 slot="middle">Github</h1>
+    <svg slot="right" aria-hidden="true" height="20" version="1.1" viewBox="0 0 14 16" width="17"><path fill-rule="evenodd" d="M14 12v1H0v-1l.73-.58c.77-.77.81-2.55 1.19-4.42C2.69 3.23 6 2 6 2c0-.55.45-1 1-1s1 .45 1 1c0 0 3.39 1.23 4.16 5 .38 1.88.42 3.66 1.19 4.42l.66.58H14zm-7 4c1.11 0 2-.89 2-2H5c0 1.11.89 2 2 2z"></path></svg>
   </page-header>
   <section class="login-form bubble">
      <h1 class="login-form-title bubble-title">Sign in to Github</h1>
@@ -37,8 +41,7 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-@import '../assets/scss/style.scss';
-@import '../assets/scss/glm.lib.css';
+@import '../assets/scss/_variables';
 
 #wrapper {
   min-height: 567px;
@@ -48,7 +51,7 @@ export default {
 }
 
 .login-form {
-  margin: 2rem;
+  margin: 2rem; 
   text-align: left;
   @at-root .login-form-title {
     color: #fff;
@@ -68,8 +71,4 @@ export default {
     border-color: #ddd;
   }
 }
-
-
-
-
 </style>
